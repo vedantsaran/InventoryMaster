1feeece91f2831d33c24944370454f62da6dcdf3
vedantsaran <108028028+vedantsaran@users.noreply.github.com>
2025-01-24 13:21:04 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,14 +1,22 @@
-
 require 'sinatra'
 require 'json'
 
-$inventory = []
-# Get all products
+
+$inventory = []      
+$suppliers = []      
+$orders = []          
+
+
+get '/' do
+  "Welcome to the InventoryMaster API!"
+end
+
+
 get '/inventory' do
   content_type :json
   $inventory.to_json
 end
-# Add a new product
+
 post '/inventory' do
   content_type :json
   data = JSON.parse(request.body.read)
@@ -21,7 +29,7 @@ post '/inventory' do
   $inventory << product
   product.to_json
 end
-# Update a product's stock
+
 put '/inventory/:id' do
   content_type :json
   id = params[:id].to_i
@@ -34,7 +42,7 @@ put '/inventory/:id' do
     halt 404, { error: 'Product not found' }.to_json
   end
 end
-# Delete a product
+
 delete '/inventory/:id' do
   content_type :json
   id = params[:id].to_i
@@ -46,3 +54,67 @@ delete '/inventory/:id' do
     halt 404, { error: 'Product not found' }.to_json
   end
 end
+
+
+get '/suppliers' do
+  content_type :json
+  $suppliers.to_json
+end
+
+post '/suppliers' do
+  content_type :json
+  data = JSON.parse(request.body.read)
+  supplier = {
+    id: ($suppliers.empty? ? 1 : $suppliers.last[:id] + 1),
+    name: data['name'],
+    contact: data['contact']
+  }
+  $suppliers << supplier
+  supplier.to_json
+end
+
+
+get '/orders' do
+  content_type :json
+  $orders.to_json
+end
+
+post '/orders' do
+  content_type :json
+  data = JSON.parse(request.body.read)
+
+ 
+  product = $inventory.find { |p| p[:id] == data['product_id'] }
+  supplier = $suppliers.find { |s| s[:id] == data['supplier_id'] }
+
+  if product && supplier
+    
+    order = {
+      id: ($orders.empty? ? 1 : $orders.last[:id] + 1),
+      product_name: product[:name],
+      quantity: data['quantity'],
+      supplier_name: supplier[:name]
+    }
+    $orders << order
+
+    
+    product[:stock] -= data['quantity'].to_i
+
+    order.to_json
+  else
+    halt 404, { error: 'Product or Supplier not found' }.to_json
+  end
+end
+
+delete '/orders/:id' do
+  content_type :json
+  id = params[:id].to_i
+  order = $orders.find { |o| o[:id] == id }
+  if order
+    $orders.delete(order)
+    { message: 'Order deleted' }.to_json
+  else
+    halt 404, { error: 'Order not found' }.to_json
+  end
+end
+
